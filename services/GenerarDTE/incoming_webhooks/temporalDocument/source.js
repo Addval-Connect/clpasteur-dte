2fc0e7aacd1e8bcbfd6d8b8735f5005ad65b2f7a
Mcode2020 <namit.mcodeinfosoft@gmail.com>
2021-04-15 13:11:23 +0530

    temporal document

--- services/GenerarDTE/incoming_webhooks/temporalDocument/source.js
+++ services/GenerarDTE/incoming_webhooks/temporalDocument/source.js
@@ -1,115 +1,74 @@
 // This function is the webhook's request handler.
 // exports({query: {servicios: '[{"monto": 1000, "codigo": 1, "nombre": "1", "cantidad": 1}]', paciente: '{"nombre": "nombre paciente", "codigo": 1, "rut": "16211150-7"}', ejecutivo: "nombre ejecutivo"}})
+
+const Joi = require('joi');
 exports = async function (payload, response) {
-    const servicesURI = decodeURIComponent(payload.query.servicios), pacienteURI = decodeURIComponent(payload.query.paciente);
-    let ejecutivoURI = decodeURIComponent(payload.query.ejecutivo), fechaBoletaURI = decodeURIComponent(payload.query.fecha);
-    const boletaExenta = context.services.get("base-dte").db(database).collection("boleta-exenta")
-    const produccion = payload.query.produccion || 0;
-    let database = produccion == 1 ? "dte-production" : "dte", retry = produccion == 1 ? 10 : 0, services = {}, paciente = {}, boletaUri = "generar-xml-uri";
-  
-    try {
-      services = JSON.parse(servicesURI);
-      paciente = JSON.parse(pacienteURI);
-    } catch (err) {
-      return { "status": "400", message: "Los parámetros de entrada no son un JSON válido." }
-    }
-  
-    for (let service of services) {
-      if (isNaN(service.monto) || isNaN(service.codigo) || !service.nombre || service.monto <= 0 || isNaN(service.cantidad) || service.cantidad <= 0) {
-        return { status: "400", message: "Uno o más valores de los servicios no son válidos." }
-      }
-    }
-  
-    if (!paciente.nombre || !paciente.codigo || !paciente.rut) {
-      return { status: "400", message: "Uno o más valores del paciente no son válidos." }
-    }
-  
-    if (typeof (ejecutivoURI) == "undefined" || !ejecutivoURI || ejecutivoURI == "") {
-      ejecutivoURI = "-";
-    }
-  
-    let ejecutivo = ejecutivoURI || "-";
-    let ultimoFolio = await boletaExenta.findOne({ "ultimoFolio": true });
-    let nuevoFolio = ultimoFolio.folio + 1;
-  
-    let body = {
-      "Encabezado": {
-        "IdDoc": {
-          "TipoDTE": 41
-        },
-        "Emisor": {
-          "RUTEmisor": "78730160-6"
-        },
-        "Receptor": {
-          "RUTRecep": paciente.rut,
-          "RznSocRecep": paciente.nombre
+    try{
+        const servicesURI = decodeURIComponent(payload.query.servicios),
+            pacienteURI = decodeURIComponent(payload.query.paciente);
+
+        let ejecutivoURI = decodeURIComponent(payload.query.ejecutivo);
+
+        const boletaExenta = context.services.get("base-dte").db(database).collection("boleta-exenta")
+        const produccion = payload.query.produccion || 0;
+
+        let database = produccion == 1 ? "dte-production" : "dte", retry = produccion == 1 ? 10 : 0,
+            servicesData = {},
+            pacienteData = {},
+            boletaUri = "generar-xml-uri";
+        const getFunction = (functionName, values) => {
+            return context.functions(functionName, ...values)
         }
-      },
-      "Detalle": [],
-      "LibreDTE": {
-        "extra": {
-          "venta": {
-            "codigoPaciente": paciente.codigo,
-            "ejecutivo": ejecutivo
-          }
+
+        try {
+            servicesData = JSON.parse(servicesURI);
+            pacienteData = JSON.parse(pacienteURI);
+        } catch (err) {
+            return { "status": "400", message: "Los parámetros de entrada no son un JSON válido." }
         }
-      }
-    };
-  
-    let total = 0;
-  
-    for (let service of services) {
-      total = total + (service.monto * service.cantidad);
-      body["Detalle"].push(
-        {
-          "IndExe": "1",
-          "NmbItem": service.nombre,
-          "QtyItem": service.cantidad,
-          "PrcItem": service.monto,
-          "CdgItem": service.codigo
-        }
-      );
-    }
-  
-    const axios = require('axios')
-  
-    let options = {
-      headers: {
-        "Accept": "application/json",
-        "Authorization": "ZXEzQ3hpeXM5V1AzOFF5MTZLUFlsMXpIZ2xqRmwxdDg6WA=="
-      }
-    }
-  
-    res = await axios.post("https://libredte.cl/api/dte/documentos/emitir?normalizar=1&formato=json&links=0&email=0", body, options);
-  
-    if (res.status == 200) {
-      bodyGenerar = {
-        "emisor": 78730160,
-        "receptor": paciente.rut,
-        "dte": 41,
-        "codigo": res.data.codigo
-      }
-  
-      let boletaEmitida = await axios.post(`https://libredte.cl/api/dte/documentos/generar?getXML=1&links=0&email=0&retry=${retry}&gzip=0`, bodyGenerar, options);
-  
-      if (boletaEmitida.status == 200) {
-        let folioBoleta = parseInt(boletaEmitida.data.folio)
-  
-        await boletaExenta.insertOne({
-          "folio": folioBoleta,
-          "servicios": services,
-          "paciente": paciente,
-          "ejecutivo": ejecutivo,
-          "libredte": boletaEmitida.data
+
+        const {
+          services,
+          paciente
+        } = getFunction("joiValidation", {
+          data: {
+            services: services,
+            paciente: paciente
+          }, schemaJson:
+            Joi.object({
+                services: Joi.array().items(
+                    Joi.object({
+                        monto:  Joi.number().required("monto"),
+                        codigo: Joi.number().required("codigo"),
+                        nombre: Joi.any().required().label("nombre"),
+                        cantidad: Joi.number().required().greater(0).label("cantidad")
+                    })
+                ).required().label("Services"),
+                paciente: Joi.object({
+                    codigo: Joi.number().required("codigo"),
+                    nombre: Joi.any().required().label("nombre"),
+                    rut: Joi.any().required().label("cantidad")
+                }).required().label("paciente"),
+            })
         });
-  
-        return { status: "200", folio: `${folioBoleta}`, monto: `${total}`, ejecutivo: ejecutivo, trackid: `${boletaEmitida.data.track_id}` };
-  
-      } else {
-        return { status: "400" }
-      }
-    } else {
-      return { status: "400" }
-    }
-  
+
+        return { status: 200 , success: "Validated"}
+  }catch(e){
+    return  { status: "400", success: e.message}
   }
+
+  /* for (let service of services) {
+    if (isNaN(service.monto) || isNaN(service.codigo) || !service.nombre || service.monto <= 0 || isNaN(service.cantidad) || service.cantidad <= 0) {
+      return { status: "400", message: "Uno o más valores de los servicios no son válidos." }
+    }
+  }
+
+  if (!paciente.nombre || !paciente.codigo || !paciente.rut) {
+    return { status: "400", message: "Uno o más valores del paciente no son válidos." }
+  } */
+
+  /* if (typeof (ejecutivoURI) == "undefined" || !ejecutivoURI || ejecutivoURI == "") {
+    ejecutivoURI = "-";
+  } */
+
+}
